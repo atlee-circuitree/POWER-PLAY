f393210181081c6768e15e8bac63d09650992368
Viassna <maskedful@gmail.com>
2022-12-05 17:57:26 -0500

    Teaching stuff road runner
    roads

--- MeepMeeptesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeeptesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,23 +9,15 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
-    int CordX = 35;
-        int Side = 1;
-
-        double CordY = -58.33333333;
-    int StartingAngle = 90;
             RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 20.24)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(CordX, CordY, 90))
-                                .lineToConstantHeading(new Vector2d(Side * 36, 6))
-                                .lineToConstantHeading(new Vector2d(Side * 36, 20))
-                                .lineToSplineHeading(new Pose2d(Side * 33, 4, Math.toRadians(270 - (75 * Side))))
-                                .strafeLeft(Side * 3.75)
-                                .forward(6)
-                                .back(4)
-                                .lineTo(new Vector2d(Side * 57, 10))
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 22, Math.toRadians(90)))
+                                .back(80)
+                                .forward(80)
+                                .strafeLeft(24)
+                                .strafeRight(48)
                                 .build()
                 );
 
